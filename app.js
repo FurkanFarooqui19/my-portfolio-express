c662e933f0583edc55b4b59aa0e58c595e943233
FurkanFarooqui19 <furkanahmedfarooqui19@gmail.com>
2026-03-11 16:31:35 +0530

    Refactor server port configuration and improve code formatting

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
 app.get("/", (req,res) => {
-    res.sendFile(__dirname+"/pages/index.html");
+    res.sendFile(__dirname + "/pages/index.html");
 });
 
 app.get("/skills", (req,res) => {
-    res.sendFile(__dirname+"/pages/my_skills.html");
+    res.sendFile(__dirname + "/pages/my_skills.html");
 });
 
-
-app.listen(port, (err) => {
-    console.log("Server started on http://localhost:"+port);
-})
+app.listen(PORT, () => {
+    console.log("Server started on port " + PORT);
+});
